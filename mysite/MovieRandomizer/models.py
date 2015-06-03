b7fbd98d66f1e1206e60f74b11d8c72791bbb411
honmok212 <honmok212@gmail.com>
2015-06-02 23:52:41 -0400

    -generated movie model -added 2 test objects in db -build test home page with randomizer

--- mysite/MovieRandomizer/models.py
+++ mysite/MovieRandomizer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Movie(models.Model):
+	title = models.CharField(max_length=200)
+	release_date = models.DateTimeField('date released')
+	#short_description = models.CharField(max_length=500)
+	#director = models.CharField(max_length=50)
+	#genre = models.CharField(max_length=50)
+	def __unicode__(self):
+		return self.title
+
 # Create your models here.
--- mysite/MovieRandomizer/views.py
+++ mysite/MovieRandomizer/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+from .models import Movie
+
+def home(request):
+	c = Movie.objects.order_by('?').first()
+	return HttpResponse(c.title)
 
 # Create your views here.
